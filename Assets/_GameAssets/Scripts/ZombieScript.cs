a0e4a9f8158056f3d8046bca8114f333918b577c
sagounitydev <43536133+sagounitydev@users.noreply.github.com>
2018-12-16 17:59:57 +0100

    Act_16_17h

---
+++ Assets/_GameAssets/Scripts/ZombieScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieScript : MonoBehaviour {
+
+    Transform zombie;
+    NavMeshAgent nav;
+
+    private Animator anim;
+
+	void Start () {
+        zombie = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bool aPorEl = true;
+        anim.SetBool("corriendo", aPorEl);
+        nav.SetDestination(zombie.position);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        bool aPorEl = false;
+        anim.SetBool("corriendo", aPorEl);
+
+    }
+}
