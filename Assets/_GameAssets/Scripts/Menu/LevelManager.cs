ab7066c81c32de6025c4f0d8a64f3f4b1d45ca5e
sagounitydev <43536133+sagounitydev@users.noreply.github.com>
2018-12-18 02:25:34 +0100

    Act_17_Dic
    Tiene menu de inicio y de fin de juego (escena 2 montada)

---
+++ Assets/_GameAssets/Scripts/Menu/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public void CargaEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CerrarJuego()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/_GameAssets/Scripts/TankScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankScript : MonoBehaviour {
+
+    public float tankForce = 50;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.Translate(0, 0, 1 * tankForce * Time.deltaTime);
+    }
+}
