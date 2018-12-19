5a3435b7a22b4a608ffffedb5918fdd5489ecda9
sagounitydev <43536133+sagounitydev@users.noreply.github.com>
2018-12-20 00:55:08 +0100

    Act_19_Dic_DesdeCasa
    Corregidos eventos audio y añadido script puntos

---
+++ Assets/_GameAssets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static int puntos;
+
+    PlayerControllerScript text;
+
+    private void Awake()
+    {
+        text = GetComponent<PlayerControllerScript>();
+        puntos = 0;
+    }
+
+    private void Update()
+    {
+        //text.puntos = "Score: " + puntos;
+    }
+}
